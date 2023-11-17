4910dc29713ef3f802b0e6d951fabd257625f179
Dmytro Sayko <saykodmytro@icloud.com>
2023-11-17 23:21:31 +0200

    up

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,8 @@
-// вставляємо boiler played code - шаблонний код
-
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { combineReducers, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { productReducer } from './products/products.reducer';
 
-// тут ми скажемо що наш rootReducer буде викликати ф-ю combineReducers і сюди ми будемо передавати обєкт редюсерів
-const rootReducer = combineReducers({
-  productsStore: productReducer,
+export const store = configureStore({
+  reducer: {
+    productsStore: productReducer,
+  },
 });
-
-const enhancer = devToolsEnhancer();
-
-// store - саме сховище, в нього передається кореневий reducer - (rootReducer)
-export const store = createStore(rootReducer, enhancer);
